a200da82d43bae55b9981321e118c4c053781810
ribeiro-jr <ribeirojr1522@gmail.com>
2023-10-13 15:18:12 -0300

    :feat: Criptografando senha do usuario

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,28 +1,36 @@
-import prismaClient from '../../prisma';
+import prismaClient from "../../prisma";
+import { hash } from "bcryptjs";
 
 // tipando type/interface
 interface UserRequest {
   name: string;
   email: string;
   password: string;
-};
+}
 
 class CreateUserService {
   async execute({ name, email, password }: UserRequest) {
-
     if (!email) {
-      throw new Error('Email incorreto!');
-    };
+      throw new Error("Email incorreto!");
+    }
 
     // verificando se no banco existe o email
     const userAlreadyExists = await prismaClient.user.findFirst({
       where: {
-        email: email
-      }
+        email: email,
+      },
     });
     if (userAlreadyExists) {
-      throw new Error('Email já cadastrado!');
-    };
+      throw new Error("Email já cadastrado!");
+    }
+
+    // criptografando senha, antes de cadastrar
+
+    /**
+     * @param {senha que será criptografada} password
+     * @param {salto de criptografia} 8
+     */
+    const passwordHash = await hash(password, 8);
 
     // cadastrando usuário
     const user = await prismaClient.user.create({
@@ -30,16 +38,15 @@ class CreateUserService {
         // o que será criado
         name: name,
         email: email,
-        password: password
+        password: passwordHash,
       },
       select: {
         //o que quero devolver
         id: true,
         name: true,
-        email: true
-      }
+        email: true,
+      },
     });
-
 
     return { user };
   }
